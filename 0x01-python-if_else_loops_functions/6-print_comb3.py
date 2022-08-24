f46f8e8a6f325d275170b3e6420f6951f09e9198
SimonM14 <kaninisimon79@gmail.com>
2022-08-24 02:39:53 +0000

    Added task

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for k in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, k))
+        else:
+            print("{}{}".format(i, k), end=", ")
+            
