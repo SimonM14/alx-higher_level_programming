940fff726eececaa802d7efb3ae6ae1f1028a046
SimonM14 <kaninisimon79@gmail.com>
2022-08-25 11:18:06 +0000

    Added new tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,12 @@
-#!/usr/bin/python3
+y#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    res = 0
-    for i in range(1, len(sys.argv)):
-        res += int(sys.argv[i])
-    print("{}".format(res))
+    num = len(sys.argv)
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument.".format(num - 1))
+    else:
+        print("{} arguments.".format(num - 1))
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
